c1bebffdfb70306bab973dbbdbb8bccd13d2dfd2
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-05 17:48:43 -0700

    [add] search form/start tennor

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, request, render_template
-from random import choice, sample
+import requests
+import json
 
 app = Flask(__name__)
+
+apikey = ""
+lmt = 10
+
 @app.route('/')
 def index():
-    """Show the homepage and ask the user's name."""
-    return render_template('index.html')
+    """Show the homepage."""
+    search = request.args.get('search')
+    print(search)
+    base_url = f"https://api.tenor.com/v1/search?q={search}&key={apikey}&limit={lmt}"
+    r = requests.get(base_url)
+
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(r.content)
+        print (top_10gifs)
+    else:
+        top_10gifs = None
+
+    return render_template('index.html', gif=r)
 
 if __name__ == "__main__":
     app.run(debug=True)
